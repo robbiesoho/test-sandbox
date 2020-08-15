b988b98b4728cd946fe9fb150bbfbe582d2cba02
robbiesoho <rob89porto@gmail.com>
2020-08-14 22:05:16 -0400

    add task delete

---
+++ www/delete.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+
+  <?php
+
+    $link = mysqli_connect("mysql", "root", "tiger", "task list");
+
+    $id = $_GET["id"];
+
+    if($link === false){
+      die("ERROR: Could not connect. " . mysqli_connect_error());
+    }
+
+    $sql = "DELETE FROM tasks WHERE task_id = ('$id')";
+    if(mysqli_query($link, $sql)){
+        echo "Task deleted!";
+    } else{
+        echo "ERROR: Could not delete task $sql. " . mysqli_error($link);
+    }
+
+    mysqli_close($link);
+
+
+  ?>
+
+
+</body>
+</html>
--- www/index.php
+++ www/index.php
@@ -13,13 +13,10 @@
         $link = mysqli_connect("mysql", "root", "tiger", "task list");
         $result = mysqli_query($link,"select * from tasks");
         while ($row = $result->fetch_assoc()){
-                echo $row["task_name"] . "<br>" ; 
+                echo $row["task_name"] . " -- ";
+                echo "<a href='delete.php?id={$row['task_id']}'>delete</a>";
+                echo "<br>"; 
         }
-
-        
-
-
-
 
     ?>
     <br>
